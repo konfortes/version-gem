5372fcdb6f429ba189d331fe54d9ce596aee6596
konfortes <ronenk@gettaxi.com>
2016-02-27 21:13:37 +0200

    added tests

--- test/test_version.rb
+++ test/test_version.rb
@@ -2,15 +2,21 @@ require 'minitest/autorun'
 require 'version'
 
 class VersionTest < Minitest::Test
-  def test_english_hello
-    assert_equal 'hello world', Hola.hi('english')
+  def test_invalid_arguments
+    assert_raises do
+      Version.in_version_range?
+    end
   end
 
-  def test_any_hello
-    assert_equal 'hello world', Hola.hi('ruby')
+  def test_greater_or_equal_than_version
+    assert Version.greater_or_equal_than_version?('6.17.0', '6.16.1')
   end
 
-  def test_spanish_hello
-    assert_equal 'hola mundo', Hola.hi('spanish')
+  def test_lesser_than_version
+    assert Version.lesser_than_version?('6.16.0', '6.17.0')
+  end
+
+  def test_in_version_range
+    assert Version.in_version_range?('6.16.0', '6.15.0', '6.17.0')
   end
 end
